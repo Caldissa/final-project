743a9cf49fd5097cf28d3f1437eb1cec64c43c80
Caldissa <kayla@prarieskies.com>
2023-11-30 14:48:47 -0600

    Modified initial desktop design

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header
-        class="absolute shadow-lg top-0 w-full bg-primary p-4 flex justify-between items-center"
+        class="absolute shadow-lg top-0 w-full h-24 bg-primary p-4 md:px-8 flex justify-between items-center"
     >
         <h1 class="text-3xl text-white tracking-widest">SWAUSays</h1>
         <i class="i-mdi:menu text-white h-8 w-8 sm:h-10 sm:w-10"></i>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="h-full w-full pt-17">
-        <div class="w-full h-full p-4 flex flex-col gap-4">
+    <div class="h-full w-full pt-24">
+        <div
+            class="w-full mx-auto max-w-prose h-full p-4 md:p-6 flex flex-col gap-4 md:gap-6"
+        >
             <Post /><Post />
         </div>
     </div>
