aa3b11f174f9336d4422e12145f1033906efbe31
Jonathan Swena <jmswena03@gmail.com>
2023-12-06 22:31:11 -0600

    Readded Header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="min-h-screen grid grid-rows-[minmax(0,1fr)_auto] w-full">
+        <Header />
         <RouterView />
     </div>
 </template>
 
 <script setup lang="ts">
-import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
 </script>
