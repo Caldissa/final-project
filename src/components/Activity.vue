e23332038a77a4270b62ef18e1ef8364fe5edb2f
Jonathan Swena <jmswena03@gmail.com>
2023-12-07 00:04:08 -0600

    Activity changes

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -2,7 +2,7 @@
     <div
         class="bg-white/15 border-l-2 border-white/50 text-white h-full w-52 p-4 md:p-6 flex flex-col gap-4 md:gap-6"
     >
-        <div class="w-full border-b-2 border-white/20">
+        <div class="w-full border-b-2 border-white/20 py-3">
             <p1>Recently Active</p1>
         </div>
         <div>Bob</div>
