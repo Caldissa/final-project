b1783586ace2e5a3f8da91762195204c428a5f69
Jonathan Swena <jmswena03@gmail.com>
2023-12-06 21:46:24 -0600

    Mainmenu Adjustments

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -2,33 +2,37 @@
     <div class="h-full w-40 p-4 md:p-6 flex flex-col gap-4 md:gap-6">
         <RouterLink
             to="/"
-            class="bg-blue-500 text-white rounded-full px-3 justify-between"
-            >Home
+            class="bg-blue-500 text-white rounded-full px-3 py-1 justify-between"
+        >
             <i class="i-mdi:home"></i>
+            Home
         </RouterLink>
         <RouterLink
             to="/profile"
-            class="bg-blue-500 text-white rounded-full px-3 text-left"
+            class="bg-blue-500 text-white rounded-full px-3 py-1 text-left"
         >
+            <i class="i-mdi:profile"></i>
             Profile
         </RouterLink>
         <a
             href="https://www.prepsportswear.com/college/us/texas/keene/southwestern-adventist-university-knights?schoolid=2062798"
-            class="bg-blue-500 text-white rounded-full px-3"
+            class="bg-blue-500 text-white rounded-full px-3 py-1"
         >
             <button class="text-left justify-between">
-                Shop <i class="i-mdi:cart"></i>
+                <i class="i-mdi:cart"></i>
+                Shop
             </button>
         </a>
         <button
-            class="bg-blue-500 text-white rounded-full px-3 text-left justify-between"
+            class="bg-blue-500 text-white rounded-full px-3 py-1 text-left justify-between"
         >
-            Post
             <i class="i-mdi:plus"></i>
+            Post
         </button>
         <button
-            class="!bg-blue-500 text-white rounded-full px-3 text-left justify-between"
+            class="!bg-blue-500 text-white rounded-full px-3 py-1 text-left justify-between"
         >
+            <i class="i-mdi:logout"></i>
             Logout
         </button>
     </div>
