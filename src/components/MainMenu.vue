9423a58b5fd2681c71b9434c74881c58e4b05b00
Jonathan Swena <jmswena03@gmail.com>
2023-12-06 21:55:31 -0600

    Mainmenu

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -2,37 +2,37 @@
     <div class="h-full w-40 p-4 md:p-6 flex flex-col gap-4 md:gap-6">
         <RouterLink
             to="/"
-            class="bg-blue-500 text-white rounded-full px-3 py-1 justify-between"
+            class="bg-secondary text-white rounded-full px-3 py-1 justify-between"
         >
-            <i class="i-mdi:home"></i>
+            <i class="i-mdi:home w-6 h-6"></i>
             Home
         </RouterLink>
         <RouterLink
             to="/profile"
-            class="bg-blue-500 text-white rounded-full px-3 py-1 text-left"
+            class="bg-secondary text-white rounded-full px-3 py-1 text-left"
         >
-            <i class="i-mdi:profile"></i>
+            <i class="i-mdi:person w-6 h-6"></i>
             Profile
         </RouterLink>
         <a
             href="https://www.prepsportswear.com/college/us/texas/keene/southwestern-adventist-university-knights?schoolid=2062798"
-            class="bg-blue-500 text-white rounded-full px-3 py-1"
+            class="bg-secondary text-white rounded-full px-3 py-1"
         >
             <button class="text-left justify-between">
-                <i class="i-mdi:cart"></i>
+                <i class="i-mdi:cart w-6 h-6"></i>
                 Shop
             </button>
         </a>
         <button
-            class="bg-blue-500 text-white rounded-full px-3 py-1 text-left justify-between"
+            class="bg-secondary text-white rounded-full px-3 py-1 text-left justify-between"
         >
-            <i class="i-mdi:plus"></i>
+            <i class="i-mdi:plus w-6 h-6"></i>
             Post
         </button>
         <button
-            class="!bg-blue-500 text-white rounded-full px-3 py-1 text-left justify-between"
+            class="bg-secondary text-white rounded-full px-3 py-1 text-left justify-between"
         >
-            <i class="i-mdi:logout"></i>
+            <i class="i-mdi:logout w-6 h-6"></i>
             Logout
         </button>
     </div>
