3ce80ae76c7541b9b51a1cc681433bd4f95a5284
Jonathan Swena <jmswena03@gmail.com>
2023-12-06 22:13:41 -0600

    removed header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
     <div class="min-h-screen grid grid-rows-[minmax(0,1fr)_auto] w-full">
-        <Header />
         <RouterView />
         <Footer />
     </div>
 </template>
 
 <script setup lang="ts">
-import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 </script>
