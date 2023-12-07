533cc0cbe3428439dcadf4530112b842a0821be4
Jonathan Swena <jmswena03@gmail.com>
2023-12-06 21:19:30 -0600

    Menu adjustments

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="h-full w-full pt-24">
-        <div
-            class="text-white w-full mx-auto max-w-prose h-full p-4 md:p-6 flex flex-col gap-4 md:gap-6"
-        >
-            FUCK OFF BITCH
+        <div class="flex-row">
+            <div>
+                <Menu></Menu>
+            </div>
+            <div
+                class="text-white w-full mx-auto max-w-prose h-full p-4 md:p-6 flex flex-col gap-4 md:gap-6"
+            ></div>
         </div>
     </div>
 </template>
+<script setup lang="ts">
+import Menu from '../components/MainMenu.vue'
+</script>
