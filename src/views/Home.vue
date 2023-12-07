7da289146ffa55b31add903e150615035053b8c8
Jonathan Swena <jmswena03@gmail.com>
2023-12-06 20:53:58 -0600

    Home Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,28 @@
 <template>
     <div class="h-full w-full pt-24">
-        <div
-            class="w-full mx-auto max-w-prose h-full p-4 md:p-6 flex flex-col gap-4 md:gap-6"
-        >
-            <Post /><Post />
+        <div class="flex flex-row">
+            <div class="h-full p-4 md:p-6 flex flex-col gap-4 md:gap-6">
+                <button class="bg-blue-500 text-white rounded-full px-3">
+                    Home
+                </button>
+                <button class="bg-blue-500 text-white rounded-full px-3">
+                    Profile
+                </button>
+                <button class="bg-blue-500 text-white rounded-full px-3">
+                    Shop
+                </button>
+                <button class="bg-blue-500 text-white rounded-full px-3">
+                    Post
+                </button>
+                <button class="!bg-blue-500 text-white rounded-full px-3">
+                    Logout
+                </button>
+            </div>
+            <div
+                class="w-full mx-auto max-w-prose h-full p-4 md:p-6 flex flex-col gap-4 md:gap-6"
+            >
+                <Post /><Post />
+            </div>
         </div>
     </div>
 </template>
---
+++ src/views/Profile.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="h-full w-full pt-24">
+        <div
+            class="w-full mx-auto max-w-prose h-full p-4 md:p-6 flex flex-col gap-4 md:gap-6"
+        >
+            <Post /><Post />
+        </div>
+    </div>
+</template>
