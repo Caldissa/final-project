e57b7b121a172e332fecbed01fe0c849f595dfaa
Jonathan Swena <jmswena03@gmail.com>
2023-12-12 10:30:46 -0600

    Initial Hamburger

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         class="h-screen overflow-y-hidden grid grid-rows-[minmax(0,1fr)_auto] w-full"
     >
         <Header />
-        <div class="h-full w-full pt-22 md:pt-0">
+        <div class="h-full w-full pt-20 md:pt-0">
             <div
                 class="grid md:grid-cols-[1fr_3fr] lg:grid-cols-[1fr_4fr_1fr] w-full h-full"
             >
@@ -12,7 +12,7 @@
                 </div>
                 <div class="w-full flex mx-auto overflow-y-scroll">
                     <div
-                        class="w-full mx-auto max-w-prose h-full p-4 md:px-6 md:py-2 xl:py-4 flex flex-col gap-4 md:gap-6"
+                        class="w-full mx-auto max-w-prose h-full md:px-6 md:py-2 xl:py-4 flex flex-col gap-4 md:gap-6"
                     >
                         <div
                             class="hidden md:flex w-full text-primary dark:text-white justify-center items-end border-b-4 border-black dark:border-primary pb-4 gap-1"
@@ -26,7 +26,9 @@
                                 ></i>
                             </div>
                         </div>
-                        <RouterView />
+                        <div class="p-4">
+                            <RouterView />
+                        </div>
                     </div>
                 </div>
                 <div class="hidden lg:flex">
