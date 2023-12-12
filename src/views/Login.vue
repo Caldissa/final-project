7a8c6d574675fcf8c72a03592524ac1aca1d22a8
IMilesP <miles.esc.24@gmail.com>
2023-12-11 19:47:18 -0600

    started login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,13 @@
 <template>
-    <div><h1>Hi</h1></div>
+    <div class="flex flex-col">
+        <h1>logo here</h1>
+        <input v-model="email" type="text" placeholder="email" />
+        <input v-model="password" type="password" placeholder="password" />
+    </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+</script>
