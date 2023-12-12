6ad8f41f6ce04aaadc9a0b1e9f56cd916590fd9c
Caldissa <kayla@prarieskies.com>
2023-12-11 20:14:49 -0600

    Changed router

---
+++ src/router/index.ts
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import dayjs from 'dayjs'
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/profile', component: () => import('../views/Profile.vue') },
+    { path: '/login', component: () => import('../views/Login.vue') }
+
+    // { path: '/shop', component: () => import('../views/Shop.vue') },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+    scrollBehavior(_to, _from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { top: 0 }
+        }
+    }
+})
+
+router.beforeEach((to, _from, next) => {
+    // if (to.path === '/login') {
+    //     return next()
+    // }
+    // const email = sessionStorage.getItem('ss_email')
+    // const timestamp = sessionStorage.getItem('ss_date')
+    // if (!email || !timestamp) {
+    //     return next('/login')
+    // }
+    // if (dayjs().diff(dayjs(timestamp), 'day') > 1) {
+    //     sessionStorage.removeItem('ss_email')
+    //     sessionStorage.removeItem('ss_date')
+    //     return next('/login')
+    // }
+    next()
+})
+
+export default router
